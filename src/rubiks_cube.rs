22985d54393af692e579b3481b3c21cfa94cffcc
Luc Drenth <luc.drenth@hotmail.com>
2024-05-23 22:32:05 +0200

    add indivindually colored cuboid faces

--- src/rubiks_cube.rs
+++ src/rubiks_cube.rs
@@ -1,14 +1,14 @@
-use bevy::{
-    prelude::*,
-    render::{
-        render_asset::RenderAssetUsages,
-        render_resource::{Extent3d, TextureDimension, TextureFormat},
-    },
-};
+use std::f32::consts::TAU;
 
-const BLOCKS_SPREAD: f32 = 0.05;
+use bevy::prelude::*;
+
+const BLOCKS_SPREAD: f32 = 0.25;
 const BLOCKS_SIZE: f32 = 1.0;
-const CUBE_SIZE: u32 = 4;
+const CUBE_SIZE: u32 = 3; // 3 for 3x3, 6 for 6x6 etc
+
+const COLOR_INSIDE_R: f32 = 0.1;
+const COLOR_INSIDE_G: f32 = 0.1;
+const COLOR_INSIDE_B: f32 = 0.1;
 
 pub struct RubiksCubePlugin;
 
@@ -20,7 +20,21 @@ impl Plugin for RubiksCubePlugin {
 }
 
 #[derive(Component)]
-struct CubeBlock;
+struct CubeBlock {
+    pub faces: [Entity; 6],
+}
+
+#[derive(Component)]
+struct CubeFace;
+
+pub enum Face {
+    Left = 0,
+    Right = 2,
+    Top = 3,
+    Bottom = 4,
+    Front = 5,
+    Back = 6,
+}
 
 fn spawn_light(mut commands: Commands) {
     commands.spawn(PointLightBundle {
@@ -39,18 +53,11 @@ fn spawn_light(mut commands: Commands) {
 fn spawn_rubiks_cube(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let mesh = meshes.add(Cuboid {
-        half_size: Vec3::ONE * BLOCKS_SIZE / 2.0,
+    let cube_face_mesh = meshes.add(Rectangle {
+        half_size: Vec2::ONE * BLOCKS_SIZE / 2.0,
     });
-    let material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(uv_debug_texture())),
-        ..default()
-    });
-
-    let spread_factor = 1.0 + BLOCKS_SPREAD;
 
     if CUBE_SIZE < 2 {
         panic!("Invalid cube size {}", CUBE_SIZE)
@@ -65,63 +72,149 @@ fn spawn_rubiks_cube(
         -(CUBE_SIZE as i32 / 2) + 1..=CUBE_SIZE as i32 / 2
     };
 
+    let spread_factor = 1.0 + BLOCKS_SPREAD;
+    let face_offset = BLOCKS_SIZE / 2.0;
+
+    let color_inside = Color::rgb(COLOR_INSIDE_R, COLOR_INSIDE_G, COLOR_INSIDE_B);
+
     for x in range.clone() {
         for y in range.clone() {
             for z in range.clone() {
-                commands.spawn((
-                    PbrBundle {
-                        mesh: mesh.clone(),
-                        material: material.clone(),
-                        transform: Transform::from_translation(
-                            Vec3::new(
-                                x as f32 * BLOCKS_SIZE - offset,
-                                y as f32 * BLOCKS_SIZE - offset,
-                                z as f32 * BLOCKS_SIZE - offset,
-                            ) * spread_factor,
-                        ),
-                        ..default()
-                    },
-                    CubeBlock,
-                ));
+                let middle_point = Vec3::new(
+                    x as f32 * BLOCKS_SIZE - offset,
+                    y as f32 * BLOCKS_SIZE - offset,
+                    z as f32 * BLOCKS_SIZE - offset,
+                ) * spread_factor;
+
+                // left
+                let mut transform =
+                    Transform::from_translation(middle_point - Vec3::new(face_offset, 0.0, 0.0));
+                transform.rotate_local_y(-TAU / 4.0);
+
+                let face_left = commands
+                    .spawn((
+                        PbrBundle {
+                            mesh: cube_face_mesh.clone(),
+                            transform: transform,
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::rgb(1.0, 0.0, 0.0),
+                                ..default()
+                            }),
+                            ..default()
+                        },
+                        CubeFace,
+                    ))
+                    .id();
+                // right
+                let mut transform =
+                    Transform::from_translation(middle_point + Vec3::new(face_offset, 0.0, 0.0));
+                transform.rotate_local_y(TAU / 4.0);
+                let face_right = commands
+                    .spawn((
+                        PbrBundle {
+                            mesh: cube_face_mesh.clone(),
+                            transform: transform,
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::rgb(0.0, 1.0, 0.0),
+                                ..default()
+                            }),
+                            ..default()
+                        },
+                        CubeFace,
+                    ))
+                    .id();
+                // top
+                let mut transform =
+                    Transform::from_translation(middle_point + Vec3::new(0.0, face_offset, 0.0));
+                transform.rotate_x(-TAU / 4.0);
+                let face_top = commands
+                    .spawn((
+                        PbrBundle {
+                            mesh: cube_face_mesh.clone(),
+                            transform: transform,
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::rgb(0.0, 0.0, 1.0),
+                                ..default()
+                            }),
+                            ..default()
+                        },
+                        CubeFace,
+                    ))
+                    .id();
+                // bottom
+                let mut transform =
+                    Transform::from_translation(middle_point - Vec3::new(0.0, face_offset, 0.0));
+                transform.rotate_x(-TAU / 4.0);
+                let face_bottom = commands
+                    .spawn((
+                        PbrBundle {
+                            mesh: cube_face_mesh.clone(),
+                            transform: transform,
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::rgb(1.0, 1.0, 0.0),
+                                ..default()
+                            }),
+                            ..default()
+                        },
+                        CubeFace,
+                    ))
+                    .id();
+                // front
+                let face_front = commands
+                    .spawn((
+                        PbrBundle {
+                            mesh: cube_face_mesh.clone(),
+                            transform: Transform::from_translation(
+                                middle_point + Vec3::new(0.0, 0.0, face_offset),
+                            ),
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::rgb(0.0, 1.0, 1.0),
+                                ..default()
+                            }),
+                            ..default()
+                        },
+                        CubeFace,
+                    ))
+                    .id();
+                // back
+                let mut transform =
+                    Transform::from_translation(middle_point - Vec3::new(0.0, 0.0, face_offset));
+                transform.rotate_local_y(-TAU / 2.0);
+                let face_back = commands
+                    .spawn((
+                        PbrBundle {
+                            mesh: cube_face_mesh.clone(),
+                            transform: transform,
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::rgb(1.0, 1.0, 0.0),
+                                ..default()
+                            }),
+                            ..default()
+                        },
+                        CubeFace,
+                    ))
+                    .id();
+
+                // commands.spawn(CubeBlock {
+                //     faces: [
+                //         face_left,
+                //         face_right,
+                //         face_top,
+                //         face_bottom,
+                //         face_front,
+                //         face_back,
+                //     ],
+                // });
             }
         }
     }
 }
 
-fn rotate_whole_cube(mut query: Query<&mut Transform, With<CubeBlock>>, time: Res<Time>) {
+fn rotate_whole_cube(mut query: Query<&mut Transform, With<CubeFace>>, time: Res<Time>) {
     for mut transform in &mut query {
         transform.rotate_around(
             Vec3::ZERO,
-            Quat::from_rotation_y(time.delta_seconds() / 3.0),
+            Quat::from_rotation_y(time.delta_seconds() / 1.5),
         );
     }
-}
-
-/// Creates a colorful test pattern
-fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-
-    let mut palette: [u8; 32] = [
-        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
-        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
-    ];
-
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
-
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-        RenderAssetUsages::RENDER_WORLD,
-    )
 }
